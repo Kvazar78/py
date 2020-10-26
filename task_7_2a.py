c5786339849cd3b90d9a5142f1a10ae142b3aca0
kvazar78 <kvazar78@mail.ru>
2020-10-26 13:02:28 +0700

    task_7_2a ready

---
+++ task_7_2a.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3.8
+
+from sys import argv
+
+conf = argv[1]
+ignore = ["duplex", "alias", "Current configuration"]
+f = open(conf, 'r')
+
+for line in f:
+    for ig in ignore:
+        if ig in line or '!' in line:
+            break
+    else:
+        print(line.rstrip())
+
